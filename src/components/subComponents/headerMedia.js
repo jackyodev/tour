1e4b6ee1628985c0957ad66458380580e35fb450
jackyodev <jackyodev@gmail.com>
2021-04-15 01:57:02 -0400

    garden css

--- src/components/subComponents/headerMedia.js
+++ src/components/subComponents/headerMedia.js
@@ -1,12 +1,13 @@
 import React from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import ReactPlayer from "react-player";
 import { Carousel } from "react-responsive-carousel";
-import YouTube from "react-youtube";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+//https://www.npmjs.com/package/react-responsive-carousel
+//https://www.npmjs.com/package/react-gallery-carousel
 
-export function headerPhoto(urlString) {
+export function plantHeaderMedia(urlString) {
   let urlArray = urlString.split(",");
 
-  //[URL] => [{src: URL}] => <div></div>
   let arrayPhotos = urlArray.map((el, i) => {
     return (
       <div key={i}>
@@ -28,22 +29,18 @@ export function gardenHeaderMedia(array) {
   let arrayMedia = array.map((el, i) => {
     if (el.media === "picture") {
       return (
-        <div key={i}>
-          <img alt="garden" src={el.media_url} />
+        <div className="gardenImage" key={i}>
+          <img alt={el.media_alt} src={el.media_url} />
         </div>
       );
     } else if (el.media === "youtube") {
-      const opts = {
-        height: "100%",
-        width: "100%",
-        controls: "1",
-      };
-
       return (
-        <div key={i} width="100%" height="100%">
-          <YouTube videoId="tkuaVkoFrJI" opts={opts} />
+        <div className="gardenVideo" key={i}>
+          <ReactPlayer controls url={el.media_url} />
         </div>
       );
+    } else {
+      return null;
     }
   });
 
@@ -55,8 +52,6 @@ export function gardenHeaderMedia(array) {
     </div>
   );
 }
-const exported = { headerPhoto };
-export default exported;
 
-//https://www.npmjs.com/package/react-responsive-carousel
-//https://www.npmjs.com/package/react-gallery-carousel
+const exported = { plantHeaderMedia, gardenHeaderMedia };
+export default exported;
