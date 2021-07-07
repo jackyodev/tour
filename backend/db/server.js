6da5b437b5ee3b000a8dad1364b60baa2d132495
jackyo <jackyo.dev@gmail.com>
2021-07-06 22:04:54 -0400

    re-modify backend server config

--- backend/db/server.js
+++ backend/db/server.js
@@ -1,7 +1,15 @@
 const pgp = require("pg-promise")({});
-const parse = require("pg-connection-string")
 
-const cn = {
+
+
+
+
+let ssl = null;
+if (process.env.NODE_ENV === 'development') {
+   ssl = {rejectUnauthorized: false};
+}
+
+const local = {
   host: "localhost",
   port: 5432,
   database: "qbgapp",
@@ -10,19 +18,23 @@ const cn = {
   max: 30, // use up to 30 connections
 };
 
-let config; 
+let config = local;
 
-if(process.env.DATABASE_URL) {
-   config = parse(process.env.DATABASE_URL);
-  config.ssl = {
-    rejectUnauthorized: false
-  };
+if (process.env.NODE_ENV) {
+config = {
+  connectionString: process.env.DATABASE_URL,
+  max: 30,
+  ssl:ssl
+};
+
 }
 
-https://www.javaniceday.com/post/pg-promise-self-signed-certificate-error-in-postgres
+//https://www.javaniceday.com/post/pg-promise-self-signed-certificate-error-in-postgres
 //https://help.heroku.com/MDM23G46/why-am-i-getting-an-error-when-i-upgrade-to-pg-8
 
-const connectionString =  config || cn;
+const connectionString =  config
+
+console.log(connectionString);
 
 const db = pgp(connectionString);
 
